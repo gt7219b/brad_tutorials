df6111133394a9ab400c8c9883de4fca3ff87192
gt7219b <bradschafer@protonmail.com>
2022-11-04 00:42:26 -0400

    first commit.

---
+++ build/turtlesim/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/gt7219b/ros2_ws/src/brad_tutorials/turtlesim
+# Build directory: /home/gt7219b/ros2_ws/src/brad_tutorials/build/turtlesim
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("turtlesim__py")
